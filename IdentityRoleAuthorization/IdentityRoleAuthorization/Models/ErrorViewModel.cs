334afde82fb770f59ee3b9c0d803660e28f4d068
Kingkaspev <kingsleychiboy22@gmail.com>
2023-11-07 15:59:54 +0100

    User Authorization and Authentiaction project

---
+++ IdentityRoleAuthorization/IdentityRoleAuthorization/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentityRoleAuthorization.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
